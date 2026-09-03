a2e3b3e76a0dfac1e376109840fb9999fa2e6dd9
Bikram <bs9872621615@gmail.com>
2026-09-03 16:00:09 +0530

    Switch PDF viewer to native iframe instead of Google gview

--- app/opportunities/[id]/PlainTestViewer.tsx
+++ app/opportunities/[id]/PlainTestViewer.tsx
@@ -24,13 +24,9 @@ export default function PlainTestViewer({ pdfUrl }: { pdfUrl: string }) {
         style={{ height: isFullscreen ? "calc(100vh - 50px)" : "80vh" }}
       >
         <iframe
-          src={`https://docs.google.com/gview?embedded=true&url=${encodeURIComponent(pdfUrl)}`}
-          className="w-full"
-          style={{
-            height: "calc(100% + 56px)",
-            marginTop: "-56px",
-            border: "none",
-          }}
+          src={`${pdfUrl}#toolbar=1`}
+          className="w-full h-full"
+          style={{ border: "none" }}
           title="Test PDF"
         />
       </div>
